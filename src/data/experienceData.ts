96be938b8a70125ad70b213a9927d72fbf3e43d3
Steven Erazo <erazoestiven1@gmail.com>
2025-05-07 21:10:33 -0500

    Update work experience

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -2,14 +2,14 @@ export const experienceData = [
   {
     title: "Asistente de TI",
     company: "Corporacion HUNEV S.A",
-    period: "2024 - 2025",
+    period: "Julio, 2024 - Marzo, 2025",
     description: "Desarrollo de una aplicación web para la gestión de firmas digitales del personal médico utilizando React, Node.js, Express y MySQL, integrando react-signature-canvas para la captura segura de firmas y aplicando principios de usabilidad. Administré servidores Linux, incluyendo servicios de correo con Postfix y máquinas virtuales mediante KVM. Colaboré en auditorías médicas automatizando flujos de trabajo con Python, integré sistemas web con software de escritorio mediante consumo de APIs, y gestioné firewalls configurando políticas de red, supervisando la infraestructura y resolviendo problemas de conectividad. También migré bases de datos de MongoDB a MySQL, depurando y estructurando los datos, mantuve páginas web en PHP implementando nuevas funcionalidades y brindé soporte técnico tanto remoto como presencial a usuarios finales.",
     technologies: ["React", "TypeScript", "Postman", "Git/Git Hub", "Tailwind CSS" , "PHP", "MySQL", "Ciberseguridad", "MongoDB","Linux"]
   },
   {
     title: "Researcher",
     company: "SmartLab - Escuela Poltecnica Nacional",
-    period: "2025 - Actualidad",
+    period: "Marzo, 2025 - Actualidad",
     description: "",
     technologies: ["Ciberseguridad", "IA", "LLMs", "Python", "Machine Learning"]
   }
