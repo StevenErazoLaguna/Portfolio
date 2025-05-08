990157667ea8ce8eb8ed0f7d3cb499cf7ee468d1
Steven Erazo <erazoestiven1@gmail.com>
2025-05-07 21:09:08 -0500

    Updates

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -1,6 +1,6 @@
 export const experienceData = [
   {
-    title: "Asistende de TI",
+    title: "Asistente de TI",
     company: "Corporacion HUNEV S.A",
     period: "2024 - 2025",
     description: "Desarrollo de una aplicación web para la gestión de firmas digitales del personal médico utilizando React, Node.js, Express y MySQL, integrando react-signature-canvas para la captura segura de firmas y aplicando principios de usabilidad. Administré servidores Linux, incluyendo servicios de correo con Postfix y máquinas virtuales mediante KVM. Colaboré en auditorías médicas automatizando flujos de trabajo con Python, integré sistemas web con software de escritorio mediante consumo de APIs, y gestioné firewalls configurando políticas de red, supervisando la infraestructura y resolviendo problemas de conectividad. También migré bases de datos de MongoDB a MySQL, depurando y estructurando los datos, mantuve páginas web en PHP implementando nuevas funcionalidades y brindé soporte técnico tanto remoto como presencial a usuarios finales.",
